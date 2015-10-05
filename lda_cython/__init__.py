17955ac5b6f0050f485a194551a0a369f8df1238
Berton Earnshaw <berton.earnshaw@gmail.com>
2015-10-05 15:29:14 -0600

    Update requirements and simplify version setting

--- lda_cython/__init__.py
+++ lda_cython/__init__.py
@@ -1,16 +1,4 @@
-from pkg_resources import get_distribution, DistributionNotFound
-import os.path
+from pkg_resources import get_distribution
 from .topic_models import (LDA, SLDA, BLSLDA, GRTM)
 
-try:
-    _dist = get_distribution('lda-cython')
-    # Normalize case for Windows systems
-    dist_loc = os.path.normcase(_dist.location)
-    here = os.path.normcase(__file__)
-    if not here.startswith(os.path.join(dist_loc, 'lda-cython')):
-        # not installed, but there is another version that *is*
-        raise DistributionNotFound
-except DistributionNotFound:
-    __version__ = 'Please install this project with setup.py'
-else:
-    __version__ = _dist.version
+__version__ = get_distribution('lda-cython').version
