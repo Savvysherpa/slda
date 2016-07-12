19c020a03a899abe5bd4c76a79258c268521142a
Matt Burbidge <mmburbidge@gmail.com>
2016-07-12 09:09:28 -0600

    version set it setup.py, not needed in __init__.py

--- slda/__init__.py
+++ slda/__init__.py
@@ -1,7 +1,1 @@
-from pkg_resources import get_distribution, DistributionNotFound
 from .topic_models import (LDA, SLDA, BLSLDA, GRTM)
-
-try:
-    __version__ = get_distribution('slda').version
-except DistributionNotFound:
-    pass
